8747c372f95737ce2a4671f8d2d3af9133002b2f
charles <charleslavong@gmail.com>
2016-07-28 13:34:56 +0200

    Adding groovy unit test support.

--- src/main/java/com/spottr/ws/Routes.java
+++ src/main/java/com/spottr/ws/Routes.java
@@ -3,14 +3,15 @@ package com.spottr.ws;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.spottr.ws.data.model.*;
+import com.spottr.ws.data.model.Athlete;
+import com.spottr.ws.data.model.Location;
+import com.spottr.ws.data.model.Wod;
 
 @Controller
 @RequestMapping("/")
---
+++ src/test/groovy/com/spottr/ws/RoutesSpec.groovy
@@ -0,0 +1,20 @@
+package com.spottr.ws;
+
+import spock.lang.*;
+
+
+class RoutesSpec extends Specification{
+
+	def before() {
+		
+	}
+	
+	def after() {
+		
+	}
+	
+	def "should be true"() {
+		expect:
+			true == true
+	}
+}
