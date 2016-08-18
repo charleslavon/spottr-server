6a28d9956f0a7412f6260b4cb004398de42cbedf
charles <charleslavong@gmail.com>
2016-08-18 11:57:32 +0200

    Adding the env configs such that the source is set on the terminal.

--- setGradle.sh
+++ setGradle.sh
@@ -8,24 +8,28 @@ sudo ln -sfn gradle-${gradle_version} /home/ubuntu/gradle/latest
 
 export GRADLE_HOME=/home/ubuntu/gradle/latest export
 echo $GRADLE_HOME
+
 echo " "
 echo " " 
 echo "current path is " 
 echo $PATH
 export PATH=${PATH%:/usr/local/heroku/bin:/home/ubuntu/.rvm/bin}
 export PATH=${PATH%/usr/local/gradle-1.10/bin}
+
 echo " "
 echo " " 
 echo "trimming ancient version of gradle from path " 
-echo $PATH
 
-export PATH=$PATH:$GRADLE_HOME/bin
+export PATH=$PATH$GRADLE_HOME/bin
 export PATH=$PATH:/usr/local/heroku/bin:/home/ubuntu/.rvm/bin
 
 echo " "
 echo " " 
 echo "final path is  " 
 echo $PATH
+
+sudo printf "export GRADLE_HOME=$GRADLE_HOME\\nexport PATH=$PATH\\n" > /home/ubuntu/gradle.sh
+source ./home/ubuntu/gradle.sh
 
 gradle -v
 
