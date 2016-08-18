48f482d47ed8786b4f12d2e28937508e9d0d82d3
charles <charleslavong@gmail.com>
2016-08-18 11:38:21 +0200

    Editing the path updates.

--- setGradle.sh
+++ setGradle.sh
@@ -1,7 +1,7 @@
 
 gradle_version=2.9
 
-wget -P /home/ubuntu/ -N https://services.gradle.org/distributions/gradle-${gradle_version}-all.zip
+wget -Pq /home/ubuntu/ -N https://services.gradle.org/distributions/gradle-${gradle_version}-all.zip
 unzip /home/ubuntu/gradle-${gradle_version}-all.zip -d /home/ubuntu/gradle
 sudo ln -sfn gradle-${gradle_version} /home/ubuntu/gradle/latest
 /home/ubuntu/gradle/latest/bin/gradle -v
@@ -12,7 +12,7 @@ echo " "
 echo " " 
 echo "current path is " 
 echo $PATH
-export PATH=${PATH%:/usr/local/heroku/bin}
+export PATH=${PATH%:/usr/local/heroku/bin:/home/ubuntu/.rvm/bin}
 export PATH=${PATH%/usr/local/gradle-1.10/bin}
 echo " "
 echo " " 
@@ -20,7 +20,7 @@ echo "trimming ancient version of gradle from path "
 echo $PATH
 
 export PATH=$PATH:$GRADLE_HOME/bin
-export PATH=$PATH:/usr/local/heroku/bin
+export PATH=$PATH:/usr/local/heroku/bin:/home/ubuntu/.rvm/bin
 
 echo " "
 echo " " 
